1cb9bce8b9d12a0069fab2b20550cc66c32b8480
Andrey Kapkaev <kapkaew@yandex.ru>
2021-08-20 13:52:10 +0300

    add path for views.add_comment

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
     path('<str:username>/<int:post_id>/', views.post_view, name="post"),
     path('<str:username>/<int:post_id>/edit/',
          views.post_edit, name="post_edit"),
+    path('<str:username>/<int:post_id>/comment/',
+         views.add_comment, name="add_comment"),
 ]
