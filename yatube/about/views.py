678273cc95505f142b7ee61fdd2e2c879a7d164d
Kapkaev Andrey <kapkaew@yandex.ru>
2022-03-04 15:39:01 +0300

    Just start

--- yatube/about/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.base import TemplateView
-
-
-class AboutAuthorView(TemplateView):
-    template_name = 'about/author.html'
-
-
-class AboutTechView(TemplateView):
-    template_name = 'about/tech.html'
--- yatube/posts/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.paginator import Paginator, Page
-from django.conf import settings
-
-
-def _get_pages(request, page_list: object) -> Page:
-    paginator = Paginator(page_list, settings.PAGINATOR_PAGE_NUM)
-    page_number = request.GET.get('page')
-    return paginator.get_page(page_number)
