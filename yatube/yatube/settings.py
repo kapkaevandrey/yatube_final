7c13456189e0fd1e39386dac24383dff128f238a
Kapkaev Andrey <kapkaew@yandex.ru>
2022-09-25 22:22:38 +0300

    Big fix

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -17,13 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v1mv8suifknir6--qy#k$tk^6%p96oq(#1bvz5p)94^q3=@@qb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '84.201.158.168',
-    'localhost',
-    'luckyyatube.tk',
-    'www.luckyyatube.tk'
+    '*'
 ]
 
 
@@ -127,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'about/static'),
-#     os.path.join(BASE_DIR, 'static')
-# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'about/static'),
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -140,7 +137,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = '/auth/login/'
 
 LOGIN_REDIRECT_URL = 'posts:index'
-# LOGOUT_REDIRECT_URL = 'index'
 
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
